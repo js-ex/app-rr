208691278a7b1bfc0a75b66e86cada975bdfcab1
azuto <azutodragon@gmail.com>
2019-05-23 22:37:32 +0200

    Dodano React Router Dodano nowe pliki Layout

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import '../styles/App.css';
+import Header from './Header';
+import Navigation from './Navigation';
+import Page from './Page';
+import Footer from './Footer';
+import { BrowserRouter as Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      SPA Router
-    </div>
+    <Route>
+      <div className="app">
+        <header className="app__header">
+          {<Header />}
+        </header>
+        <main className="app__main">
+          <aside className="app__nav">
+            {<Navigation />}
+          </aside>
+          <section className="app__page"><Page /></section>
+        </main>
+        <footer className="app__footer">{<Footer />}</footer>
+      </div>
+    </Route>
   );
 }
 
