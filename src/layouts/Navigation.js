9d4d5cc768a0917e9b27da6955b5591c4f7cb387
azuto <azutodragon@gmail.com>
2019-05-23 22:58:46 +0200

    Dodano funkcjonalności do komponentu nawigacji Dodano stylowanie nawigacji

--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import '../styles/Navigation.css';
+import { NavLink } from 'react-router-dom';
+
+const list = [
+    { name: "Home", path: "/", exact: true },
+    { name: "Produkty", path: "/products" },
+    { name: "Kontakt", path: "/contact" },
+    { name: "Admin", path: "/admin" },
+]
+
+const menu = list.map(item => (
+    <li key={item.name}>
+        <NavLink to={item.path} exact={item.exact ? item.exact : false}>{item.name}</NavLink>
+    </li>
+))
 
 const Navigation = () => {
     return (
-        <div>Navigation</div>
+        <nav>
+            {menu ? <ul>{menu}</ul> : null}
+        </nav>
     );
 }
 
