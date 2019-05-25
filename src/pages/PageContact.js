d63ce2d0c59be474d5d5970a460ab872b97f1c7c
azuto <azutodragon@gmail.com>
2019-05-25 20:21:15 +0200

    Dodano obsluge formularza na stronie "Kontakt", wykorzystano komponent Router -> Prompt

--- src/pages/PageContact.js
+++ src/pages/PageContact.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import { Prompt } from 'react-router-dom';
+import '../styles/PageContact.css';
 
-const PageContact = () => {
-    return (
-        <div>
-            <h1>Skontaktuj się z nami</h1>
-        </div>
-    );
+class PageContact extends React.Component {
+
+    state = {
+        value: "",
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        this.setState({
+            value: ""
+        });
+
+    }
+
+    handleChange = (e) => {
+
+        this.setState({
+            value: e.target.value
+        });
+
+    }
+
+    render() {
+
+        return (
+            <div>
+                <h1>Skontaktuj się z nami</h1>
+                <form className="app__form" onSubmit={this.handleSubmit}>
+                    <label htmlFor="app__form" aria-label="Treść wiadomości">
+                        <textarea className="app__form-text" id="form__form" placeholder="Treść wiadomości" onChange={this.handleChange} value={this.state.value}></textarea>
+                    </label>
+                    <button className="app__form-btn">Wyślij formularz</button>
+                </form>
+                <Prompt when={this.state.value} message="Na pewno chesz opuścić stronę z wypełnionym formularzem?" />
+            </div >
+        );
+
+    }
+
 }
 
 export default PageContact;
