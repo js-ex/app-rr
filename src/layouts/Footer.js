bc72720eefee5069760fa6781ca7c3b7218a7271
azuto <azutodragon@gmail.com>
2019-05-26 11:52:17 +0200

    Dodano rozudowe stopki o pobieranie i wyswietlanie danych routingu (url, id)

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import '../styles/Footer.css';
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div>
+            <h2>Footer</h2>
+            <Route path="/:page/" render={({ match }) => {
+                return <p>Jesteś na stronie: <strong>{match.params.page}</strong></p>
+            }} />
+            <Route path="/:page/:id" render={({ match }) => {
+                console.log(match);
+                return <p>Jesteś na podstronie: <strong>{match.params.page}</strong> / <strong>{match.params.id}</strong></p>
+            }} />
+        </div>
     );
 }
 
