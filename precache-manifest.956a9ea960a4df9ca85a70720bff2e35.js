35aa364ed82c8e9941499cc0196afcb3ce8457fe
azuto <azutodragon@gmail.com>
2019-05-27 21:47:27 +0200

    Updates

---
+++ precache-manifest.956a9ea960a4df9ca85a70720bff2e35.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "512c0c8143bc724e9e6e5b17e69e200b",
+    "url": "/app-rr/index.html"
+  },
+  {
+    "revision": "a4415d4d8eafed78d4ce",
+    "url": "/app-rr/static/css/main.1a3da4e8.chunk.css"
+  },
+  {
+    "revision": "150339bc785a64f81052",
+    "url": "/app-rr/static/js/2.cacc07d3.chunk.js"
+  },
+  {
+    "revision": "a4415d4d8eafed78d4ce",
+    "url": "/app-rr/static/js/main.6b9416e1.chunk.js"
+  },
+  {
+    "revision": "9347ed20ee693a8d157a",
+    "url": "/app-rr/static/js/runtime~main.fe775957.js"
+  },
+  {
+    "revision": "ddd4f6cd6f2228b4681aa39802d88ced",
+    "url": "/app-rr/static/media/image__1.ddd4f6cd.jpg"
+  },
+  {
+    "revision": "998eed2dfd92d224eb30e0409f28015f",
+    "url": "/app-rr/static/media/image__2.998eed2d.jpg"
+  },
+  {
+    "revision": "7ffaa06367d6a211c6b00a8cdc6fa22f",
+    "url": "/app-rr/static/media/image__3.7ffaa063.jpg"
+  },
+  {
+    "revision": "72ca7ea19e5e6dcff5fc38d1a83b0f9a",
+    "url": "/app-rr/static/media/image__4.72ca7ea1.jpg"
+  }
+]);
